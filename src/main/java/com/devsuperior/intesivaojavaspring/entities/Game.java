67fc00ad7bfbcca9bcd050a2a9fd0d59f6c2951a
Carlos Fraga <vittorfraga@outlook.com>
2023-05-11 01:43:15 -0300

    Game seed

--- src/main/java/com/devsuperior/intesivaojavaspring/entities/Game.java
+++ src/main/java/com/devsuperior/intesivaojavaspring/entities/Game.java
@@ -15,9 +15,15 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String  plataform;
+    private String  platforms;
+    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
+
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){}
@@ -36,15 +42,24 @@ public class Game {
     }
 
 
-    public Game(Long id, String title, Integer year, String genre, String plataform, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.plataform = plataform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public Long getId() {
@@ -79,12 +94,12 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlataform() {
-        return plataform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlataform(String plataform) {
-        this.plataform = plataform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
     }
 
     public String getImgUrl() {
