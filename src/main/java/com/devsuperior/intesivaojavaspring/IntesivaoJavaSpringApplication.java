e60eec1be0b2793ff1d4ac183a1c6201d7c045bf
Carlos Fraga <vittorfraga@outlook.com>
2023-05-11 00:04:19 -0300

    projet created

---
+++ src/main/java/com/devsuperior/intesivaojavaspring/IntesivaoJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.intesivaojavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntesivaoJavaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntesivaoJavaSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devsuperior/intesivaojavaspring/IntesivaoJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.intesivaojavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntesivaoJavaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
